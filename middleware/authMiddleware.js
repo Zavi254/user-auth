1430244f11bca30028271fa58c036682a6a906d7
Steven Otieno <steveotianga8991@gmail.com>
2024-05-31 02:36:02 +0300

    implemented the bearer token issue

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,18 +1,17 @@
-// middleware/authMiddleware.js
-
 const jwt = require("jsonwebtoken");
 require('dotenv').config()
 
 module.exports = (req, res, next) => {
   // Get token from header
-  const token = req.header("x-auth-token");
+  const authHeader = req.header("Authorization");
 
   // Check if not token
-  if (!token) {
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ msg: "No token, authorization denied" });
   }
 
   // Verify token
+  const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
