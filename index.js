9c037158bae8950e030a4cddfd0ddf61bcd5361e
Steven Otieno <steveotianga8991@gmail.com>
2024-05-31 01:12:02 +0300

    Fixed the Db error

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const { mongoURI } = process.env.MONGODB_URI;
+const mongoURI = process.env.MONGODB_URI
 
 // Middleware
 app.use(cors());
